74bd5fae24df90ff64a19ea258947d3206ad304b
dimnass3 <dimnasekarlangitpuruhito@mail.ugm.ac.id>
2026-09-16 23:32:26 +0700

    revisi1

--- Problem2.cpp
+++ Problem2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     string pesan;
 
     cout << "Masukkan pesan: ";
@@ -9,31 +9,20 @@ int main() {
 
     cout << "Hasil sandi: ";
 
-    // Huruf pertama tetap
     cout << pesan[0];
 
-    for (int i = 1; i < pesan.length(); i++) {
-
-        // Nilai huruf sekarang
+    for(int i = 1; i < pesan.length(); i++){
         int sekarang = pesan[i] - 'A' + 1;
-
-        // Nilai huruf sebelumnya
         int sebelumnya = pesan[i - 1] - 'A' + 1;
-
-        // Jumlahkan
         int hasil = sekarang + sebelumnya;
 
-        // Kalau lebih dari 26, kembali dari A
-        if (hasil > 26) {
+        if(hasil > 26){
             hasil = hasil - 26;
         }
-
-        // Ubah angka kembali menjadi huruf
         char huruf = 'A' + hasil - 1;
 
         cout << huruf;
     }
-
     cout << endl;
 
     return 0;
